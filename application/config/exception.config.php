e46bdb9b54db546c4e2a088f596421f8c7f77c36
MrCyph3r <mrcyph3r@users.noreply.github.com>
2015-01-24 20:56:02 +0100

    Add a few basic config settings.

---
+++ application/config/exception.config.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'handle_with_controller' => false,
+    'handle_controller' => 'ErrorController@handleException', // Format is Controller@Method
+    'log_exception' => false
+);
---
+++ application/config/router.config.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+    'handle_with_controller' => true,
+    'handle_controller' => 'ErrorController@error404'
+);
