ed190f7e828acfec063e03a81ec8eef91dd6ef40
MrCyph3r <mrcyph3r@users.noreply.github.com>
2015-04-24 22:59:41 +0200

    Add string.helper.php DocBlocks.

--- vendor/alice/framework/src/Support/string.helper.php
+++ vendor/alice/framework/src/Support/string.helper.php
@@ -2,6 +2,13 @@
 
 if (!function_exists('startsWith'))
 {
+    /**
+     * This method is used to check if a string starts with a particular substring.
+     *
+     * @param string $haystack The string to check.
+     * @param string $needle   The substring to search for.
+     * @return bool            True if $haystack starts with $needle, False otherwise.
+     */
     function startsWith($haystack, $needle)
     {
         return strrpos($haystack, $needle, -strlen($haystack)) === 0;
@@ -10,6 +17,13 @@ if (!function_exists('startsWith'))
 
 if (!function_exists('endsWith'))
 {
+    /**
+     * This method is used to check if a string ends with a particular substring.
+     *
+     * @param string $haystack The string to check.
+     * @param string $needle   The substring to search for.
+     * @return bool            True if $haystack ends with $needle, False otherwise.
+     */
     function endsWith($haystack, $needle)
     {
         return ($needle === '') ? false : (($offset = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $offset) !== FALSE);
